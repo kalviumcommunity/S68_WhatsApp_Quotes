eff7e95e6d60f8b2e1bd7ca128384b1a6a695a3d
mayur-driod <settymayurk@gmail.com>
2025-02-17 09:46:53 +0530

    created-react

---
+++ frontend/src/Pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <>
+        <h1>Cringe Whatsapp quotes!</h1>
+        <p>
+            So this is a ridiculous project as specified in the question... I find it extremely hilarious how people put up quotes on their WhatsApp descriptions. So based on the user logging in it would show a list of the top 10 quotes that I find funny or interesting. I think that I can have a pre-determined set of top 10 quotes and every time a user logs in I can randomly pick a set of 10 quotes and go through them in something like a carousel that scrolls every second revealing the quotes from 10(last) to 1(top). The purpose of this website is for people to just get a good laugh.
+        </p>
+    </>
+  )
+}
+
+export default Landing
